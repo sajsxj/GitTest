8f8c11d3cd67a178d8ce23193d5f0764acf96535
saj <sajsxj@nyist.edu.cn>
2020-02-01 16:46:45 +0800

    first to commit

---
+++ src/com/saj/test/Test.java
@@ -0,0 +1,9 @@
+package com.saj.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
